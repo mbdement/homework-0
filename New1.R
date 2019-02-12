c2b416fbc52b7013f992a57b91e85a87f7bf812c
mbdement <madement@gmail.com>
2019-02-12 19:28:55 +0100

    Udemi R basics Kirill video sec2lecture8

---
+++ New1.R
@@ -0,0 +1,22 @@
+
+
+#comments
+# to run press cntr+enter
+
+#integer x=2, L makes it Integer, good for categorial variables
+x <- 2L
+typeof(x)
+
+#double
+y <- 2.5
+typeof(y) #returns type of variable y
+
+#complex
+z <- 3+2i
+
+#caracter
+a <- "h"
+
+#logical
+q1 <- TRUE #TRUE or just T 
+
