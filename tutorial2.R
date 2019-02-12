01c2667ede897d129e2600c3bae51fd47d1907c2
mbdement <madement@gmail.com>
2019-02-12 19:51:58 +0100

    lectures 8 and 9

---
+++ tutorial2.R
@@ -0,0 +1,38 @@
+
+
+A <- 10
+B <- 5
+
+C <- A + B
+C #prints the value of C
+
+var1 <- 2.5
+var2 <- 4
+
+#comments
+
+result <- var1/var2
+result
+
+answer <- sqrt(var2)
+
+#charaacter operations
+
+greeting <- "Hello"
+name <- "Bob"
+message <- paste(greeting, name)
+
+#logical
+# FALSE F
+#TRUE T
+
+4<5
+10 > 100
+4==5 #equal
+# not equal !=
+# <=
+# >=
+# ! means a simple "not"
+
+result  <- 4< 5
+typeof(result)
